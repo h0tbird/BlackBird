41a62cf8087f0e0a42fc025a530ae77d5111de5a
Marc Villacorta <marc.villacorta@gmail.com>
2011-02-10 20:41:36 +0100

    Copyright notice and copying permission statement.

--- src/BlackBird.c
+++ src/BlackBird.c
@@ -1,6 +1,23 @@
-//-----------------------------------------------------------------------------
-// gcc -Wall -pthread BlackBird.c
-//-----------------------------------------------------------------------------
+/******************************************************************************
+* Copyright (C) 2011 Marc Villacorta Morera
+*
+* Authors: Marc Villacorta Morera <marc.villacorta@gmail.com>
+*
+* This file is part of BlackBird.
+*
+* BlackBird is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* BlackBird is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with BlackBird. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 //-----------------------------------------------------------------------------
 // Includes:
